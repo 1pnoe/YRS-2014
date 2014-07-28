0919becd97a0517876185f6f4029c1764b43b654
Ollie <o.copleston@gmail.com>
2014-07-28 17:01:00 +0100

    Updated request

--- pokeme.py
+++ pokeme.py
@@ -3,16 +3,40 @@
 #Importing the appropriate lib
 
 import requests
+import pprint
 
+name = ""
+national_id = ""
+types = ""
+attack = ""
+defence = ""
+sp_atk = ""
+sp_def = ""
+speed = ""
+total = ""
 
-body = {"name": name,
-        "national_id": national_id,
-        "types": types,
-        "attack": attack,
-        "defence": defence,
-        "sp_atk": sp_atk,
-        "sp_def": sp_def,
-        "speed": speed,
-        "total": total}
+#body = {"name": name,
+ #       "national_id": national_id,
+  #      "types": types,
+   #     "attack": attack,
+    #    "defence": defence,
+     #   "sp_atk": sp_atk,
+      #  "sp_def": sp_def,
+       # "speed": speed,
+        #"total": total}
 
-response = requests.post("http://pokeapi.co/api/v1/pokemon/", 
+#Entry from user on website
+pokemon_id = "1/"
+
+#The URL of the API
+endpoint = "http://pokeapi.co/api/v1/pokemon/"
+
+#Requesting a response from the API using the given "pokemon_id"
+response = requests.get( endpoint, params=pokemon_id)
+
+#variable which stores that data from the API request
+data= response.json
+
+pprint.pprint(data)
+
+print (data.name)
