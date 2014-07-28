8aa6eedd7b8ebe4fd58ae01e031e988f7a3fe6d9
Ollie <o.copleston@gmail.com>
2014-07-28 14:53:25 +0100

    Switched to requests lib

--- pokeme.py
+++ pokeme.py
@@ -2,4 +2,17 @@
 
 #Importing the appropriate lib
 
-from urlib2 import urlopen
+import requests
+
+
+body = {"name": name,
+        "national_id": national_id,
+        "types": types,
+        "attack": attack,
+        "defence": defence,
+        "sp_atk": sp_atk,
+        "sp_def": sp_def,
+        "speed": speed,
+        "total": total}
+
+response = requests.post("http://pokeapi.co/api/v1/pokemon/", 
